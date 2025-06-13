42d0dcea1839eb5dc8d08f601a15e44f0db959a7
coehgns <modooboiroo@gmail.com>
2025-06-13 20:01:03 +0900

    chore(#1): project setting

---
+++ src/main/kotlin/team/jeonghokim/zakbu/ZakBuBeApplication.kt
@@ -0,0 +1,11 @@
+package team.jeonghokim.zakbu
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ZakBuBeApplication
+
+fun main(args: Array<String>) {
+	runApplication<ZakBuBeApplication>(*args)
+}
---
+++ src/test/kotlin/team/jeonghokim/zakbu/ZakBuBeApplicationTests.kt
@@ -0,0 +1,13 @@
+package team.jeonghokim.zakbu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ZakBuBeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
