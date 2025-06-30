53f69c7ab0c2c8eaff86746ec4d2e2b6161090ac
coehgns <modooboiroo@gmail.com>
2025-06-30 12:15:50 +0900

    feat(#4): GlobalExceptionHandler

---
+++ src/main/kotlin/team/jeonghokim/zakbu/global/error/GlobalExceptionHandler.kt
@@ -0,0 +1,32 @@
+package team.jeonghokim.zakbu.global.error
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import team.jeonghokim.zakbu.global.error.excpetion.ZakBuException
+
+@RestControllerAdvice
+class GlobalExceptionHandler() {
+
+    @ExceptionHandler(ZakBuException::class)
+    fun handlingPickException(e: ZakBuException): ResponseEntity<ErrorResponse> {
+        val code = e.errorCode
+        return ResponseEntity(
+            ErrorResponse(code.status, code.message),
+            HttpStatus.valueOf(code.status)
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun validatorExceptionHandler(e: MethodArgumentNotValidException): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(
+            ErrorResponse(
+                400,
+                e.bindingResult.allErrors[0].defaultMessage!!
+            ),
+            HttpStatus.BAD_REQUEST
+        )
+    }
+}
