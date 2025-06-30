c3b4464189d0eed3b4c866d2301cd47566881d07
coehgns <modooboiroo@gmail.com>
2025-06-30 11:34:14 +0900

    build(#2): DependencyVersion

---
+++ buildSrc/src/main/kotlin/DependencyVersion.kt
@@ -0,0 +1,9 @@
+object DependencyVersion {
+    const val KOTLIN = "1.9.25"
+    const val SPRING_BOOT = "3.4.4"
+    const val SPRING_DEPENDENCY_MANAGEMENT = "1.1.7"
+    const val KTLINT = "12.1.1"
+
+    const val JWT = "0.11.5"
+    const val ORG_JSON = "20230227"
+}
