df5e31b3d02a7b98f2659b9a00cae734fdc6b237
coehgns <modooboiroo@gmail.com>
2025-06-30 11:34:07 +0900

    build(#2): PluginVersions

---
+++ buildSrc/src/main/kotlin/PluginVersions.kt
@@ -0,0 +1,9 @@
+object PluginVersions {
+    const val KOTLIN_VERSION = "1.9.25"
+    const val SPRING_BOOT_VERSION = "3.4.4"
+    const val JPA_PLUGIN_VERSION = "1.9.25"
+    const val KAPT_VERSION = "1.7.10"
+    const val JVM_VERSION = "1.9.25"
+    const val SPRING_DEPENDENCY_MANAGEMENT_VERSION = "1.1.7"
+    const val KTLINT_VERSION = "12.1.1"
+}
