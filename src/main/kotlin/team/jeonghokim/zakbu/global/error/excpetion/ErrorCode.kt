a4c1fa77a9d8dc4f3f56193cfb1601fd2e92fc37
민수아 <Minsua0503@gmail.com>
2025-07-02 14:39:28 +0900

    feat(#7): Jwt 관련 Exception 추가

--- src/main/kotlin/team/jeonghokim/zakbu/global/error/excpetion/ErrorCode.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/error/excpetion/ErrorCode.kt
@@ -3,4 +3,8 @@ package team.jeonghokim.zakbu.global.error.excpetion
 enum class ErrorCode(
     val status: Int,
     val message: String
-)
+) {
+    // jwt
+    EXPIRED_JWT(401, "Expired JWT"),
+    INVALID_JWT(401, "Invalid JWT"),
+}
