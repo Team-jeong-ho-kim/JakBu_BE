312a276b1f55c3a840660a8b71c8f6f1c44e090f
민수아 <Minsua0503@gmail.com>
2025-07-02 14:39:07 +0900

    feat(#7): JwtTokenFilter

---
+++ src/main/kotlin/team/jeonghokim/zakbu/global/security/jwt/JwtTokenFilter.kt
@@ -0,0 +1,29 @@
+package team.jeonghokim.zakbu.global.security.jwt
+
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletException
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+
+class JwtTokenFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+): OncePerRequestFilter() {
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val parseToken: String? = jwtTokenProvider.resolveToken(request)
+
+        if (parseToken != null) {
+            val authentication: Authentication = jwtTokenProvider.authentication(parseToken)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+}
