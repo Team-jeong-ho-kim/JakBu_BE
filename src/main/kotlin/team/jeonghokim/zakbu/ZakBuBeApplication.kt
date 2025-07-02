3d60d18d84e9d3968d931e152c773372ea5183dc
민수아 <Minsua0503@gmail.com>
2025-07-02 15:12:40 +0900

    chore(#7): @ConfigurationPropertiesScan 추가

--- src/main/kotlin/team/jeonghokim/zakbu/ZakBuBeApplication.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/ZakBuBeApplication.kt
@@ -1,9 +1,11 @@
 package team.jeonghokim.zakbu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class ZakBuBeApplication
 
 fun main(args: Array<String>) {
