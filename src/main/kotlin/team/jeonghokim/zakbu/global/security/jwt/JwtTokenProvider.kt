4b74a3d365b816f65b79d9e6b9f991c7035a5267
민수아 <Minsua0503@gmail.com>
2025-07-02 14:33:22 +0900

    feat(#7): JwtTokenProvider

---
+++ src/main/kotlin/team/jeonghokim/zakbu/global/security/jwt/JwtTokenProvider.kt
@@ -0,0 +1,80 @@
+package team.jeonghokim.zakbu.global.security.jwt
+
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.userdetails.UserDetails
+import team.jeonghokim.zakbu.domain.auth.domain.RefreshToken
+import team.jeonghokim.zakbu.domain.auth.domain.exception.ExpiredJwtException
+import team.jeonghokim.zakbu.domain.auth.domain.exception.InvalidJwtException
+import team.jeonghokim.zakbu.domain.auth.domain.repository.RefreshTokenRepository
+import team.jeonghokim.zakbu.global.security.auth.AuthDetailsService
+import java.util.*
+import javax.crypto.spec.SecretKeySpec
+
+class JwtTokenProvider(
+    private val jwtProperties: JwtProperties,
+    private val authDetailsService: AuthDetailsService,
+    private val secretKeySpec: SecretKeySpec,
+    private val refreshTokenRepository: RefreshTokenRepository
+) {
+    private fun generateToken(id: UUID, type: String, exp: Long): String {
+        return Jwts.builder()
+            .signWith(secretKeySpec)
+            .setSubject(id.toString())
+            .setHeaderParam("type", type)
+            .setIssuedAt(Date())
+            .setExpiration(Date(System.currentTimeMillis() + exp * 1000))
+            .compact()
+    }
+
+    fun generateAccessToken(id: UUID): String {
+        return generateToken(id, "access", jwtProperties.accessExp)
+    }
+
+    fun generateRefreshToken(id: UUID): String {
+        val refreshToken = generateToken(id, "refresh", jwtProperties.refreshExp)
+        refreshTokenRepository.save(
+            RefreshToken(id, refreshToken, jwtProperties.refreshExp)
+        )
+        return refreshToken
+    }
+
+    fun parseToken(bearerToken: String?): String? {
+        if (bearerToken != null && bearerToken.startsWith(jwtProperties.prefix)) {
+            return bearerToken.replace(jwtProperties.prefix, "").trim()
+        }
+        return null
+    }
+
+    fun resolveToken(request: HttpServletRequest): String? {
+        val bearerToken = request.getHeader(jwtProperties.header)
+        return parseToken(bearerToken)
+    }
+
+    private fun getClaims(token: String): Claims {
+        return try {
+            Jwts.parserBuilder()
+                .setSigningKey(secretKeySpec)
+                .build()
+                .parseClaimsJws(token)
+                .body
+        } catch (e: Exception) {
+            when (e) {
+                is io.jsonwebtoken.ExpiredJwtException -> throw ExpiredJwtException
+                else -> throw InvalidJwtException
+            }
+        }
+    }
+
+    private fun getTokenSubject(token: String): String {
+        return getClaims(token).subject
+    }
+
+    fun authentication(token: String): Authentication {
+        val userDetails: UserDetails = authDetailsService.loadUserByUsername(getTokenSubject(token))
+        return UsernamePasswordAuthenticationToken(userDetails, "", userDetails.authorities)
+    }
+}
