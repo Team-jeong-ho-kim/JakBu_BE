aaed97e9f67d1bb8b61619749550ffe622503f92
민수아 <Minsua0503@gmail.com>
2025-07-02 15:13:22 +0900

    feat(#7): @ConfigurationProperties 추가

--- src/main/kotlin/team/jeonghokim/zakbu/global/security/jwt/JwtProperties.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/security/jwt/JwtProperties.kt
@@ -1,5 +1,8 @@
 package team.jeonghokim.zakbu.global.security.jwt
 
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "spring.jwt")
 class JwtProperties(
     val secretKey: String,
     val header: String,
