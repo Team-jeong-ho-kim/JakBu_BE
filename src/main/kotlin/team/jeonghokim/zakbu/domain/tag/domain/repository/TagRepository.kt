84a42570e53b7e573516ace562581ddc8a7ccb45
coehgns <modooboiroo@gmail.com>
2025-07-03 22:45:54 +0900

    chore(#6): TagRepository 삭제

--- src/main/kotlin/team/jeonghokim/zakbu/domain/tag/domain/repository/TagRepository.kt
+++
@@ -1,7 +0,0 @@
-package team.jeonghokim.zakbu.domain.tag.domain.repository
-
-import org.springframework.data.jpa.repository.JpaRepository
-import team.jeonghokim.zakbu.domain.tag.domain.Tag
-import java.util.UUID
-
-interface TagRepository : JpaRepository<Tag, UUID>
