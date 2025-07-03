1fd4529e83878ab6ad8b35524ed25e6b39dc8de1
민수아 <Minsua0503@gmail.com>
2025-07-03 19:21:06 +0900

    refactor(#7): @ConfigurationPropertiesScan 제거

--- src/main/kotlin/team/jeonghokim/zakbu/ZakBuBeApplication.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/ZakBuBeApplication.kt
@@ -1,11 +1,9 @@
 package team.jeonghokim.zakbu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-@ConfigurationPropertiesScan
 class ZakBuBeApplication
 
 fun main(args: Array<String>) {
