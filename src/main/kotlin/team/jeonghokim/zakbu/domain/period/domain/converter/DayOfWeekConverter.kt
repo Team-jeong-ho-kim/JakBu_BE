de5a80fd1676a53e5f13875cf52c7dd05010b1f6
coehgns <modooboiroo@gmail.com>
2025-07-03 09:16:23 +0900

    feat(#6): DayOfWeekConverter

---
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/period/domain/converter/DayOfWeekConverter.kt
@@ -0,0 +1,17 @@
+package team.jeonghokim.zakbu.domain.period.domain.converter
+
+import jakarta.persistence.AttributeConverter
+import jakarta.persistence.Converter
+import java.time.DayOfWeek
+
+@Converter
+internal class DayOfWeekConverter : AttributeConverter<DayOfWeek, Int> {
+
+    override fun convertToDatabaseColumn(attribute: DayOfWeek): Int {
+        return attribute.value
+    }
+
+    override fun convertToEntityAttribute(dbData: Int): DayOfWeek {
+        return dbData.let { DayOfWeek.of(it) }
+    }
+}
