241175651209c74618777f080b5449b081bd6ae6
coehgns <modooboiroo@gmail.com>
2025-07-04 12:17:31 +0900

    refactor(#6): conflict

--- src/main/kotlin/team/jeonghokim/zakbu/global/error/exception/ErrorCode.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/error/exception/ErrorCode.kt
@@ -2,5 +2,9 @@ package team.jeonghokim.zakbu.global.error.exception
 
 enum class ErrorCode(
     val status: Int,
-    val message: String,
-)
+    val message: String
+) {
+    // jwt
+    EXPIRED_JWT(401, "Expired JWT"),
+    INVALID_JWT(401, "Invalid JWT"),
+}
