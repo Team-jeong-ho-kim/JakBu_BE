cebbf4a5aecccd006f57a128485ac399d1c49e20
coehgns <modooboiroo@gmail.com>
2025-07-04 12:09:39 +0900

    refactor(#6): BaseEntity lateinit 설정

--- src/main/kotlin/team/jeonghokim/zakbu/domain/BaseEntity.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/BaseEntity.kt
@@ -8,13 +8,13 @@ import org.hibernate.annotations.UuidGenerator
 import java.util.*
 
 @MappedSuperclass
-abstract class BaseEntity(
+abstract class BaseEntity() : BaseTimeEntity() {
     @Id
     @GeneratedValue(generator = "uuid2")
     @UuidGenerator
     @Column(
         columnDefinition = "BINARY(16)",
-        nullable = false,
+        nullable = false
     )
-    val id: UUID?,
-) : BaseTimeEntity()
+    lateinit var id: UUID
+}
