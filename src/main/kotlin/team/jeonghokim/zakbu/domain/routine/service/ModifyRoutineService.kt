7039109ac58b20af78995f6b29cd200e3c3c71e2
coehgns <modooboiroo@gmail.com>
2025-07-04 23:06:58 +0900

    feat(#13): ModifyRoutineService에 RoutineFacade 적용

--- src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/ModifyRoutineService.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/ModifyRoutineService.kt
@@ -4,18 +4,18 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.jeonghokim.zakbu.domain.period.domain.Period
 import team.jeonghokim.zakbu.domain.routine.domain.repository.RoutineRepository
-import team.jeonghokim.zakbu.domain.routine.exception.RoutineNotFoundException
+import team.jeonghokim.zakbu.domain.routine.facade.RoutineFacade
 import team.jeonghokim.zakbu.domain.routine.presentation.dto.request.RoutineRequest
 import java.util.*
 
 @Service
 class ModifyRoutineService(
+    private val routineFacade: RoutineFacade,
     private val routineRepository: RoutineRepository
 ) {
     @Transactional
     fun execute(routineId: UUID, request: RoutineRequest) {
-        val routine = routineRepository.findRoutineById(routineId)
-            ?: throw RoutineNotFoundException
+        val routine = routineFacade.getRoutineById(routineId)
 
         routine.modifyRoutine(
             routineName = request.routineName,
