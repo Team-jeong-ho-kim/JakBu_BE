28eb43868f396c643139505a4f160b9f2a99d9f5
coehgns <modooboiroo@gmail.com>
2025-07-04 12:16:03 +0900

    refactor(#6): BaseEntity

--- src/main/kotlin/team/jeonghokim/zakbu/domain/BaseEntity.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/BaseEntity.kt
@@ -1,20 +1,14 @@
 package team.jeonghokim.zakbu.domain
 
-import jakarta.persistence.Column
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
 import jakarta.persistence.MappedSuperclass
-import org.hibernate.annotations.UuidGenerator
-import java.util.*
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import java.time.LocalDateTime
 
 @MappedSuperclass
-abstract class BaseEntity() : BaseTimeEntity() {
-    @Id
-    @GeneratedValue(generator = "uuid2")
-    @UuidGenerator
-    @Column(
-        columnDefinition = "BINARY(16)",
-        nullable = false
-    )
-    lateinit var id: UUID
-}
+abstract class BaseEntity(
+    @CreatedDate
+    val createdAt: LocalDateTime = LocalDateTime.now(),
+    @LastModifiedDate
+    val modifiedAt: LocalDateTime = LocalDateTime.now(),
+) : BaseUUIDEntity()
