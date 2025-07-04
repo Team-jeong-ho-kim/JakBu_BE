e84d6929b79fe012ff9c0d1016086e833e72d29e
coehgns <modooboiroo@gmail.com>
2025-07-04 20:56:35 +0900

    refactor(#11): ModifyRoutineService

--- src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/ModifyRoutineService.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/ModifyRoutineService.kt
@@ -2,6 +2,7 @@ package team.jeonghokim.zakbu.domain.routine.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import team.jeonghokim.zakbu.domain.period.domain.Period
 import team.jeonghokim.zakbu.domain.routine.domain.repository.RoutineRepository
 import team.jeonghokim.zakbu.domain.routine.exception.RoutineNotFoundException
 import team.jeonghokim.zakbu.domain.routine.presentation.dto.request.ModifyRoutineRequest
@@ -16,7 +17,23 @@ class ModifyRoutineService(
         val routine = routineRepository.findRoutineById(routineId)
             ?: throw RoutineNotFoundException
 
-        routine.modifyRoutine(request)
+        routine.modifyRoutine(
+            routineName = request.routineName,
+            goal = request.goal,
+            iconUrl = request.iconUrl,
+            importance = request.importance,
+            status = request.status,
+            startDate = request.startDate,
+            isCompleted = request.isCompleted,
+            tag = request.tag,
+            period = request.period.run {
+                Period(
+                    repeat = repeat,
+                    activeDay = activeDay
+                )
+            }
+        )
+
         routineRepository.save(routine)
     }
 }
