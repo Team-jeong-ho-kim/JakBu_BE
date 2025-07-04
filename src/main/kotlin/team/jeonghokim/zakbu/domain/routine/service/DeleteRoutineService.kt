1b7b4ec68a249f4ed3704b7e3e7b9b6ef5322ce9
coehgns <modooboiroo@gmail.com>
2025-07-04 23:07:02 +0900

    feat(#13): DeleteRoutineService RoutineFacade 적용

--- src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/DeleteRoutineService.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/DeleteRoutineService.kt
@@ -3,18 +3,17 @@ package team.jeonghokim.zakbu.domain.routine.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.jeonghokim.zakbu.domain.routine.domain.repository.RoutineRepository
-import team.jeonghokim.zakbu.domain.routine.exception.RoutineNotFoundException
+import team.jeonghokim.zakbu.domain.routine.facade.RoutineFacade
 import java.util.*
 
 @Service
 class DeleteRoutineService(
+    private val routineFacade: RoutineFacade,
     private val routineRepository: RoutineRepository
 ) {
     @Transactional
     fun execute(routineId: UUID) {
-        val routine = routineRepository.findRoutineById(routineId)
-            ?: throw RoutineNotFoundException
-
+        val routine = routineFacade.getRoutineById(routineId)
         routineRepository.delete(routine)
     }
 }
