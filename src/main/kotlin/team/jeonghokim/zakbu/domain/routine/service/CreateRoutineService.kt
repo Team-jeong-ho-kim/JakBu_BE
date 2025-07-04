776f942cfbfb6a6a41c18145f605cf88c0acdf2f
coehgns <modooboiroo@gmail.com>
2025-07-04 11:16:35 +0900

    refactor(#6): period 저장 로직 삭제

--- src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/CreateRoutineService.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/CreateRoutineService.kt
@@ -2,7 +2,6 @@ package team.jeonghokim.zakbu.domain.routine.service
 
 import org.springframework.stereotype.Service
 import team.jeonghokim.zakbu.domain.period.domain.Period
-import team.jeonghokim.zakbu.domain.period.domain.repository.PeriodRepository
 import team.jeonghokim.zakbu.domain.routine.domain.Routine
 import team.jeonghokim.zakbu.domain.routine.domain.repository.RoutineRepository
 import team.jeonghokim.zakbu.domain.routine.presentation.dto.request.CreateRoutineRequest
@@ -10,7 +9,6 @@ import team.jeonghokim.zakbu.domain.routine.presentation.dto.request.CreateRouti
 @Service
 class CreateRoutineService(
     private val routineRepository: RoutineRepository,
-    private val periodRepository: PeriodRepository
 ) {
     fun execute(request: CreateRoutineRequest) {
         val period = request.period.run {
@@ -35,6 +33,5 @@ class CreateRoutineService(
         }
 
         routineRepository.save(routine)
-        periodRepository.save(period)
     }
 }
