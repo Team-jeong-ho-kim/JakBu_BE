9b58881a0c8f772249695af90034b3bf7725098a
coehgns <modooboiroo@gmail.com>
2025-07-04 20:25:55 +0900

    feat(#11): RoutineRepository에 findRoutineById 메서드 추가

--- src/main/kotlin/team/jeonghokim/zakbu/domain/routine/domain/repository/RoutineRepository.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/routine/domain/repository/RoutineRepository.kt
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 import team.jeonghokim.zakbu.domain.routine.domain.Routine
 import java.util.UUID
 
-interface RoutineRepository : JpaRepository<Routine, UUID>
+interface RoutineRepository : JpaRepository<Routine, UUID> {
+    fun findRoutineById(routineId: UUID): Routine?
+}
