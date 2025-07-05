c63486a87dfdd57859890889e21ae80755223d0a
민수아 <Minsua0503@gmail.com>
2025-07-05 22:28:25 +0900

    refactor(#9): 공백 제거

--- src/main/kotlin/team/jeonghokim/zakbu/global/oauth/info/google/GoogleOauth2UserInfo.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/oauth/info/google/GoogleOauth2UserInfo.kt
@@ -1,12 +1,12 @@
-package team.jeonghokim.zakbu.global.oauth.info
+package team.jeonghokim.zakbu.global.oauth.info.google
 
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import team.jeonghokim.zakbu.global.oauth.info.Oauth2UserInfo
 import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 
 class GoogleOauth2UserInfo(
     private val accessToken: String,
     private val attributes: Map<String, Any>
-
 ): Oauth2UserInfo {
     private val email: String = attributes["email"] as? String
         ?: throw OAuth2AuthenticationException("Google OAuth 응답에 email이 없습니다.")
