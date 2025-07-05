0b8ccdf7f9bdfe96daa828f55c6891e1fc1a4041
민수아 <Minsua0503@gmail.com>
2025-07-05 09:10:44 +0900

    feat(#9): from 메서드 추가

--- src/main/kotlin/team/jeonghokim/zakbu/global/oauth/provider/Oauth2Provider.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/oauth/provider/Oauth2Provider.kt
@@ -2,5 +2,10 @@ package team.jeonghokim.zakbu.global.oauth.provider
 
 enum class Oauth2Provider(val registrationId: String) {
     GOOGLE("google"),
-    KAKAO("kakao")
+    KAKAO("kakao");
+
+    companion object {
+        fun from(registrationId: String): Oauth2Provider? =
+            entries.find { it.registrationId == registrationId }
+    }
 }
