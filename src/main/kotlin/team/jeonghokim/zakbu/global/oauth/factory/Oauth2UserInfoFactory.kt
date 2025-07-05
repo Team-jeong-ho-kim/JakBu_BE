34c38a19a23d7caa6840cc35dbd894a6693b9c11
민수아 <Minsua0503@gmail.com>
2025-07-05 09:11:04 +0900

    feat(#9): Oauth2UserInfoFactory

---
+++ src/main/kotlin/team/jeonghokim/zakbu/global/oauth/factory/Oauth2UserInfoFactory.kt
@@ -0,0 +1,23 @@
+package team.jeonghokim.zakbu.global.oauth.factory
+
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import team.jeonghokim.zakbu.global.oauth.info.GoogleOauth2UserInfo
+import team.jeonghokim.zakbu.global.oauth.info.Oauth2UserInfo
+import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
+
+object Oauth2UserInfoFactory {
+    fun getOauth2UserInfo(
+        registrationId: String,
+        accessToken: String,
+        attributes: Map<String, Any>
+    ): Oauth2UserInfo {
+        val provider = Oauth2Provider.from(registrationId)
+            ?: throw OAuth2AuthenticationException("지원하지 않는 Oauth 제공자: $registrationId")
+
+        return when (provider) {
+            Oauth2Provider.GOOGLE -> GoogleOauth2UserInfo(accessToken, attributes)
+            // 카카오 추가
+            else -> throw OAuth2AuthenticationException("지원하지 않는 OAuth2 제공자: $provider")
+        }
+    }
+}
