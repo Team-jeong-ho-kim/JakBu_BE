050917cdd6a4faab062e9b993fcdf1cf4bce06de
coehgns <modooboiroo@gmail.com>
2025-07-06 22:52:32 +0900

    refactor(#19): GetAllRoutineService의 IsCompleted를 completed로 수정

--- src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/GetAllRoutineService.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/routine/service/GetAllRoutineService.kt
@@ -3,7 +3,7 @@ package team.jeonghokim.zakbu.domain.routine.service
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import team.jeonghokim.zakbu.domain.routine.domain.repository.RoutineRepository
-import team.jeonghokim.zakbu.domain.routine.presentation.dto.response.GetAllRoutineResponse
+import team.jeonghokim.zakbu.domain.routine.presentation.dto.response.GetRoutinesResponse
 import team.jeonghokim.zakbu.domain.routine.presentation.dto.response.RoutineResponse
 
 @Service
@@ -11,13 +11,13 @@ class GetAllRoutineService(
     private val routineRepository: RoutineRepository
 ) {
     @Transactional(readOnly = true)
-    fun execute(): GetAllRoutineResponse {
-        return GetAllRoutineResponse(
+    fun execute(): GetRoutinesResponse {
+        return GetRoutinesResponse(
             routineRepository.findAll().map { routine ->
                 RoutineResponse(
                     routine.id,
                     routine.routineName,
-                    routine.isCompleted
+                    routine.completed
                 )
             }
         )
