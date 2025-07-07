39306bc3ebd0e27b61564bdb46636ed3618ea6df
민수아 <Minsua0503@gmail.com>
2025-07-07 15:47:58 +0900

    feat(#9): Oauth2FailureHandler

---
+++ src/main/kotlin/team/jeonghokim/zakbu/global/oauth/handler/Oauth2FailureHandler.kt
@@ -0,0 +1,19 @@
+package team.jeonghokim.zakbu.global.oauth.handler
+
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.MediaType
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler
+import org.springframework.stereotype.Component
+import java.io.IOException
+
+@Component
+class Oauth2FailureHandler: SimpleUrlAuthenticationFailureHandler() {
+    @Throws(IOException::class)
+    override fun onAuthenticationFailure(request: HttpServletRequest, response: HttpServletResponse, exception: AuthenticationException) {
+        response.contentType = MediaType.APPLICATION_JSON_VALUE
+        response.characterEncoding = "UTF-8"
+        response.writer.write(exception.message.toString())
+    }
+}
