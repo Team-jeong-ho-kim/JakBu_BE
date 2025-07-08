cfaf670ef8c6204e7c917c98aa8f3cb3ee9939f0
민수아 <Minsua0503@gmail.com>
2025-07-08 10:02:56 +0900

    refactor(#9): private 제거

--- src/main/kotlin/team/jeonghokim/zakbu/domain/user/domain/User.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/user/domain/User.kt
@@ -10,22 +10,18 @@ import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 @Entity(name = "tbl_user")
 class User(
     @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(255)")
-    private val email: String,
+    val email: String,
 
     @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(10)")
-    private val userName: String,
+    val userName: String,
 
     @Column(columnDefinition = "VARCHAR(20)")
-    private val promise: String? = null,
+    val promise: String? = null,
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, columnDefinition = "VARCHAR(6)")
-    private val oauthProvider: Oauth2Provider,
+    val oauthProvider: Oauth2Provider,
 
     @Column(nullable = false, columnDefinition = "CHAR(255)")
-    private val deviceToken: String = ""
-) : BaseUUIDEntity() {
-    fun getEmail(): String {
-        return email
-    }
-}
+    val deviceToken: String = ""
+) : BaseUUIDEntity()
