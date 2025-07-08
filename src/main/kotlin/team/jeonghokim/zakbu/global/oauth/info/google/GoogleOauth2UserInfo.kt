49001717d5974d6d91d52a0533351eb8e377f5f6
민수아 <Minsua0503@gmail.com>
2025-07-08 15:10:30 +0900

    refactor(#21): 예외처리 방식 변경

--- src/main/kotlin/team/jeonghokim/zakbu/global/oauth/info/google/GoogleOauth2UserInfo.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/oauth/info/google/GoogleOauth2UserInfo.kt
@@ -1,6 +1,7 @@
 package team.jeonghokim.zakbu.global.oauth.info.google
 
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException
+import team.jeonghokim.zakbu.global.exception.oauth.OauthEmailNotFoundException
+import team.jeonghokim.zakbu.global.exception.oauth.OauthUserNameNotFoundException
 import team.jeonghokim.zakbu.global.oauth.info.Oauth2UserInfo
 import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 
@@ -9,10 +10,10 @@ class GoogleOauth2UserInfo(
     private val attributes: Map<String, Any>
 ) : Oauth2UserInfo {
     private val email: String = attributes["email"] as? String
-        ?: throw OAuth2AuthenticationException("Google OAuth 응답에 email이 없습니다.")
+        ?: throw OauthEmailNotFoundException
 
     private val userName: String = attributes["name"] as? String
-        ?: throw OAuth2AuthenticationException("Google Oauth 응답에 name이 없습니다.")
+        ?: throw OauthUserNameNotFoundException
 
     private val provider: Oauth2Provider = Oauth2Provider.GOOGLE
 
