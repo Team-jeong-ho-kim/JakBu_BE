151285bcc570a2de3effd67bb49ed6d4cdbee87b
coehgns <modooboiroo@gmail.com>
2025-07-09 23:34:17 +0900

    chore(#19): User Entity conflict 해결

--- src/main/kotlin/team/jeonghokim/zakbu/domain/user/domain/User.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/user/domain/User.kt
@@ -2,35 +2,26 @@ package team.jeonghokim.zakbu.domain.user.domain
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import java.util.*
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import team.jeonghokim.zakbu.global.entity.BaseUUIDEntity
+import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 
 @Entity(name = "tbl_user")
 class User(
-    @Id
-    @GeneratedValue
-    private val id: UUID? = null,
-
     @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(255)")
-    private val email: String,
-
-    @Column(nullable = false, columnDefinition = "CHAR(60)")
-    private val password: String,
+    val email: String,
 
     @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(10)")
-    private val userName: String,
+    val userName: String,
 
     @Column(columnDefinition = "VARCHAR(20)")
-    private val promise: String ?,
+    val promise: String? = null,
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false, columnDefinition = "VARCHAR(6)")
-    private val oauthProvider: String,
+    val oauthProvider: Oauth2Provider,
 
     @Column(nullable = false, columnDefinition = "CHAR(255)")
-    private val deviceToken: String
-) {
-    fun getEmail(): String {
-        return email
-    }
-}
+    val deviceToken: String = ""
+) : BaseUUIDEntity()
