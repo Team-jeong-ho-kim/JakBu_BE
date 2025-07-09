27a88e7e6ddf39e007300753be9c0a7de6f157b3
민수아 <Minsua0503@gmail.com>
2025-07-09 22:16:35 +0900

    feat(#21): KakaoOauth2UserInfo

---
+++ src/main/kotlin/team/jeonghokim/zakbu/global/oauth/info/kakao/KakaoOauth2UserInfo.kt
@@ -0,0 +1,36 @@
+package team.jeonghokim.zakbu.global.oauth.info.kakao
+
+import team.jeonghokim.zakbu.global.exception.oauth.OauthEmailNotFoundException
+import team.jeonghokim.zakbu.global.exception.oauth.OauthKakaoAccountNotFoundException
+import team.jeonghokim.zakbu.global.exception.oauth.OauthKakaoProfileNotFoundException
+import team.jeonghokim.zakbu.global.exception.oauth.OauthUserNameNotFoundException
+import team.jeonghokim.zakbu.global.oauth.info.Oauth2UserInfo
+import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
+
+class KakaoOauth2UserInfo(
+    private val attributes: Map<String, Any> = emptyMap()
+) : Oauth2UserInfo {
+    private val kakaoAccount = attributes["kakao_account"] as? Map<*, *>
+        ?: throw OauthKakaoAccountNotFoundException
+
+    private val kakaoProfile = kakaoAccount["profile"] as? Map<*, *>
+        ?: throw OauthKakaoProfileNotFoundException
+
+    private val email: String = kakaoAccount["email"] as? String
+        ?: throw OauthEmailNotFoundException
+
+    private val userName: String = kakaoProfile["nickname"] as? String
+        ?: throw OauthUserNameNotFoundException
+
+    override fun getEmail(): String {
+        return email
+    }
+
+    override fun getUsername(): String {
+        return userName
+    }
+
+    override fun getProvider(): Oauth2Provider {
+        return Oauth2Provider.KAKAO
+    }
+}
