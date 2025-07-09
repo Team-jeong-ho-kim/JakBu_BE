be396ccb592765306bbafc1d00b018afd232350f
coehgns <modooboiroo@gmail.com>
2025-07-09 23:36:41 +0900

    chore(#19): AuthDetails conflict 해결

--- src/main/kotlin/team/jeonghokim/zakbu/global/security/auth/AuthDetails.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/security/auth/AuthDetails.kt
@@ -2,11 +2,21 @@ package team.jeonghokim.zakbu.global.security.auth
 
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.oauth2.core.user.OAuth2User
 import team.jeonghokim.zakbu.domain.user.domain.User
+import team.jeonghokim.zakbu.global.exception.oauth.EmailNotFoundException
+import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 
 class AuthDetails(
-    private val user: User
-) : UserDetails {
+    private val user: User?,
+    private val attributes: Map<String, Any> = emptyMap(),
+    private val isRegistered: Boolean = false,
+    private val oauth2Provider: Oauth2Provider = Oauth2Provider.UNKNOWN
+) : UserDetails, OAuth2User {
+    fun isRegistered() = isRegistered
+
+    fun getOauth2Provider(): Oauth2Provider = oauth2Provider
+
     override fun getAuthorities(): Collection<GrantedAuthority?> {
         return emptyList()
     }
@@ -16,7 +26,9 @@ class AuthDetails(
     }
 
     override fun getUsername(): String {
-        return user.getEmail()
+        return user?.email
+            ?: attributes["email"] as? String
+            ?: throw EmailNotFoundException
     }
 
     override fun isAccountNonExpired(): Boolean {
@@ -33,5 +45,20 @@ class AuthDetails(
 
     override fun isEnabled(): Boolean {
         return true
+    }
+
+    override fun <A> getAttribute(name: String): A {
+        @Suppress("UNCHECKED_CAST")
+        return this.attributes[name] as A
+    }
+
+    override fun getAttributes(): Map<String, Any> {
+        return this.attributes
+    }
+
+    override fun getName(): String {
+        return user?.email
+            ?: (attributes["email"] as? String)
+            ?: throw EmailNotFoundException
     }
 }
