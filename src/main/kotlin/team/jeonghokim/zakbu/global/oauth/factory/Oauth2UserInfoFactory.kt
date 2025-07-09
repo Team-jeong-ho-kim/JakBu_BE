f7f60800a2c2c745f59341191a5919c38f8d42c0
민수아 <Minsua0503@gmail.com>
2025-07-09 22:17:10 +0900

    feat(#21): Kakao 추가

--- src/main/kotlin/team/jeonghokim/zakbu/global/oauth/factory/Oauth2UserInfoFactory.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/global/oauth/factory/Oauth2UserInfoFactory.kt
@@ -3,6 +3,7 @@ package team.jeonghokim.zakbu.global.oauth.factory
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException
 import team.jeonghokim.zakbu.global.oauth.info.google.GoogleOauth2UserInfo
 import team.jeonghokim.zakbu.global.oauth.info.Oauth2UserInfo
+import team.jeonghokim.zakbu.global.oauth.info.kakao.KakaoOauth2UserInfo
 import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 
 object Oauth2UserInfoFactory {
@@ -16,7 +17,7 @@ object Oauth2UserInfoFactory {
 
         return when (provider) {
             Oauth2Provider.GOOGLE -> GoogleOauth2UserInfo(accessToken, attributes)
-            // 카카오 추가
+            Oauth2Provider.KAKAO -> KakaoOauth2UserInfo(accessToken, attributes)
             else -> throw OAuth2AuthenticationException("지원하지 않는 OAuth2 제공자: $provider")
         }
     }
