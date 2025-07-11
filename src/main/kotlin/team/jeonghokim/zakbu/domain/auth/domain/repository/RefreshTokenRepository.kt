b5c077a26c99267c62a6af9f0bf809a7a8cc3271
민수아 <Minsua0503@gmail.com>
2025-07-11 20:51:54 +0900

    feat(#26): findByRefreshToken 쿼리 메서드 추가

--- src/main/kotlin/team/jeonghokim/zakbu/domain/auth/domain/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/auth/domain/repository/RefreshTokenRepository.kt
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository
 import team.jeonghokim.zakbu.domain.auth.domain.RefreshToken
 import java.util.UUID
 
-interface RefreshTokenRepository : CrudRepository<RefreshToken, UUID>
+interface RefreshTokenRepository : CrudRepository<RefreshToken, UUID> {
+    fun findByRefreshToken(refreshToken: String): RefreshToken?
+}
