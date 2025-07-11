538f35d481d38aedcaea8905c0ba71cc297d5d5e
민수아 <Minsua0503@gmail.com>
2025-07-11 20:17:45 +0900

    refactor(#24): 유효성 검사 어노테이션 변경

--- src/main/kotlin/team/jeonghokim/zakbu/domain/auth/presentation/dto/request/SignupRequest.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/auth/presentation/dto/request/SignupRequest.kt
@@ -1,6 +1,7 @@
 package team.jeonghokim.zakbu.domain.auth.presentation.dto.request
 
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 
@@ -15,6 +16,6 @@ data class SignupRequest(
     @field:Size(max = 20, message = "다짐은 20자 이내로 입력해주세요.")
     val promise: String,
 
-    @field:NotBlank
+    @field:NotNull
     val oauthProvider: Oauth2Provider
 )
