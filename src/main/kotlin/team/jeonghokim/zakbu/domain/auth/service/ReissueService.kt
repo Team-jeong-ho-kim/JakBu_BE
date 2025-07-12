2a2b5772ab52dcab52ac90392c13be0e791f59d8
민수아 <Minsua0503@gmail.com>
2025-07-12 12:56:44 +0900

    refactor(#26): 관심사 분리

--- src/main/kotlin/team/jeonghokim/zakbu/domain/auth/service/ReissueService.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/auth/service/ReissueService.kt
@@ -3,6 +3,7 @@ package team.jeonghokim.zakbu.domain.auth.service
 import jakarta.servlet.http.HttpServletRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import team.jeonghokim.zakbu.domain.auth.domain.RefreshToken
 import team.jeonghokim.zakbu.domain.auth.domain.repository.RefreshTokenRepository
 import team.jeonghokim.zakbu.domain.auth.exception.RefreshTokenNotFoundException
 import team.jeonghokim.zakbu.domain.auth.presentation.dto.response.TokenResponse
@@ -19,20 +20,36 @@ class ReissueService(
 ) {
     @Transactional
     fun execute(request: HttpServletRequest): TokenResponse {
-        val refreshToken = request.getHeader(jwtProperties.header)
-            ?: throw InvalidJwtException
+        val refreshToken = extractTokenFromRequest(request)
 
-        val parseToken = jwtTokenProvider.parseToken(refreshToken)
-            ?: throw InvalidJwtException
+        val parseToken = parseToken(refreshToken)
 
-        val redisRefreshToken = refreshTokenRepository.findByRefreshToken(parseToken)
-            ?: throw RefreshTokenNotFoundException
+        val redisRefreshToken = getRefreshToken(parseToken)
 
-        val accessToken = redisRefreshToken.email
+        val email = redisRefreshToken.email
         refreshTokenRepository.delete(redisRefreshToken)
 
-        val newAccessToken = jwtTokenProvider.generateAccessToken(accessToken)
-        val newRefreshToken = jwtTokenProvider.generateRefreshToken(newAccessToken)
+        return createToken(email)
+    }
+
+    private fun extractTokenFromRequest(request: HttpServletRequest): String {
+        return request.getHeader(jwtProperties.header)
+            ?: throw InvalidJwtException
+    }
+
+    private fun parseToken(refreshToken: String): String {
+        return jwtTokenProvider.parseToken(refreshToken)
+            ?: throw InvalidJwtException
+    }
+
+    private fun getRefreshToken(parseToken: String) : RefreshToken {
+        return refreshTokenRepository.findByRefreshToken(parseToken)
+            ?: throw RefreshTokenNotFoundException
+    }
+
+    private fun createToken(email: String): TokenResponse {
+        val newAccessToken = jwtTokenProvider.generateAccessToken(email)
+        val newRefreshToken = jwtTokenProvider.generateRefreshToken(email)
 
         return TokenResponse(
             accessToken = newAccessToken,
