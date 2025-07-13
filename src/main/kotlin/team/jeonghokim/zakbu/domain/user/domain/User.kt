d0320c229f66e04436ab717b5fd2d6391a8b38dc
민수아 <Minsua0503@gmail.com>
2025-07-13 21:15:56 +0900

    refactor(#30): val -> var로 변경

--- src/main/kotlin/team/jeonghokim/zakbu/domain/user/domain/User.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/user/domain/User.kt
@@ -10,18 +10,18 @@ import team.jeonghokim.zakbu.global.oauth.provider.Oauth2Provider
 @Entity(name = "tbl_user")
 class User(
     @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(255)")
-    val email: String,
+    var email: String,
 
     @Column(unique = true, nullable = false, columnDefinition = "VARCHAR(10)")
-    val userName: String,
+    var userName: String,
 
     @Column(columnDefinition = "VARCHAR(20)")
-    val promise: String? = null,
+    var promise: String? = null,
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, columnDefinition = "VARCHAR(6)")
-    val oauthProvider: Oauth2Provider,
+    var oauthProvider: Oauth2Provider,
 
     @Column(nullable = false, columnDefinition = "CHAR(255)")
-    val deviceToken: String = ""
+    var deviceToken: String = ""
 ) : BaseUUIDEntity()
