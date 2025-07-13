67d644cc1c7f04337ec734c7624c51863d9b0d30
민수아 <Minsua0503@gmail.com>
2025-07-13 21:47:27 +0900

    feat(#30): UpdateUserRequest

---
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/user/presentation/dto/request/UpdateUserRequest.kt
@@ -0,0 +1,16 @@
+package team.jeonghokim.zakbu.domain.user.presentation.dto.request
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Size
+
+data class UpdateUserRequest(
+    @field:NotBlank
+    val email: String,
+
+    @field:NotBlank
+    @field:Size(max = 10)
+    val userName: String,
+
+    @field:Size(max = 20)
+    val promise: String
+)
