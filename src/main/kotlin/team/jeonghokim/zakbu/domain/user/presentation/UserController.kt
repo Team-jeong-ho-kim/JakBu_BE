d9fc8439e494b701f5ad80e634c128f37d25798b
민수아 <Minsua0503@gmail.com>
2025-07-13 22:47:49 +0900

    feat(#32): SetDeviceTokenService

--- src/main/kotlin/team/jeonghokim/zakbu/domain/user/presentation/UserController.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/user/presentation/UserController.kt
@@ -1,18 +1,32 @@
 package team.jeonghokim.zakbu.domain.user.presentation
 
+import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import team.jeonghokim.zakbu.domain.user.presentation.dto.request.SetDeviceTokenRequest
 import team.jeonghokim.zakbu.domain.user.presentation.dto.response.UserResponse
 import team.jeonghokim.zakbu.domain.user.service.GetUserService
+import team.jeonghokim.zakbu.domain.user.service.SetDeviceTokenService
 
 @RestController
 @RequestMapping("/users")
 class UserController(
-    private val getUserService: GetUserService
+    private val getUserService: GetUserService,
+    private val setDeviceTokenService: SetDeviceTokenService
 ) {
     @GetMapping
     fun getUser(): UserResponse {
         return getUserService.execute()
+    }
+
+    @PatchMapping("/token")
+    fun setToken(
+        @RequestBody @Valid
+        request: SetDeviceTokenRequest
+    ) {
+        setDeviceTokenService.execute(request)
     }
 }
