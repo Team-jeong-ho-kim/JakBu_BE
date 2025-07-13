be45727c42cb334fea386e70be7465c2d1c90ec1
민수아 <Minsua0503@gmail.com>
2025-07-13 22:23:23 +0900

    feat(#30): validateEmail 추가

--- src/main/kotlin/team/jeonghokim/zakbu/domain/user/service/UpdateUserService.kt
+++ src/main/kotlin/team/jeonghokim/zakbu/domain/user/service/UpdateUserService.kt
@@ -2,17 +2,28 @@ package team.jeonghokim.zakbu.domain.user.service
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import team.jeonghokim.zakbu.domain.user.domain.repository.UserRepository
+import team.jeonghokim.zakbu.domain.user.exception.EmailAlreadyInUseException
 import team.jeonghokim.zakbu.domain.user.facade.UserFacade
 import team.jeonghokim.zakbu.domain.user.presentation.dto.request.UpdateUserRequest
 
 @Service
 class UpdateUserService(
-    private val userFacade: UserFacade
+    private val userFacade: UserFacade,
+    private val userRepository: UserRepository
 ) {
     @Transactional
     fun execute(request: UpdateUserRequest) {
         val user = userFacade.getUser()
 
+        validateEmail(request.email)
+
         user.update(request.email, request.userName, request.promise)
+    }
+
+    private fun validateEmail(email: String) {
+        if (userRepository.existsUserByEmail(email)) {
+            throw EmailAlreadyInUseException
+        }
     }
 }
